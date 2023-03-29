cf88139dd4a955bc84813f47cb0f58d60e54090a
Horatiu Roman <horatiuromantic@gmail.com>
2023-03-29 14:07:04 +0200

    fixed server to not use damn canvas

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const WebSocket = require('ws');
-const express = require('express');
-
-// start an express server on port 80
-/*const app = express();
-app.use(express.static('public'));
-app.listen(80, () => {
-  console.log('Express server started on port 80');
-});*/
-
+// canvas isn't working. we should include the package
+//const canvas = require('canvas');
 
 const PORT = (process.env.PORT || 8080);
 
@@ -17,14 +10,6 @@ const server = new WebSocket.Server({ port: PORT });
 // Handle incoming connections
 server.on('connection', (socket) => {
   console.log('Client connected');
-
-  // Send current canvas state to new client
-  const imageData = canvas.toDataURL();
-  const canvasStateMessage = {
-    type: 'canvasState',
-    imageData: imageData
-  };
-  socket.send(JSON.stringify(canvasStateMessage));
 
   // Handle incoming messages
   socket.on('message', (message) => {
@@ -45,3 +30,5 @@ server.on('connection', (socket) => {
 });
 
 console.log(`WebSocket server started on port ${PORT}`);
+
+// deployed to: https://draw-test-1.herokuapp.com/
